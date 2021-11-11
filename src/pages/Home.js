04e5dbd4f46dbae2327e088fb876a58505695778
Amir Ahmadi <eamahma@gmail.com>
2021-11-10 20:16:02 -0500

    add state and enter city name

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { useLazyQuery } from "@apollo/client";
 import { GET_WEATHER_QUERY } from "../graphql/Queries";
 
 function Home() {
+
+    const [citySearched, setCitySearched] = useState("");
     const [getWeather, { loading, data, error }] = useLazyQuery(GET_WEATHER_QUERY, {
-        variables: { name: "Vancouver"},
+        // variables: { name: "Vancouver"},
+        variables: { name: citySearched},
     })
 
     if (error) return <h1>Error found</h1>
@@ -14,7 +17,7 @@ function Home() {
     return (
         <div className="home">
             <h1>Search for Weather</h1>
-            <input type="text" placeholder="City name ...."/>      
+            <input type="text" placeholder="City name ...." onChange = {(event) => {setCitySearched(event.target.value)}}/>      
             <button onClick={() => getWeather() }>Search</button>      
         </div>
     )
