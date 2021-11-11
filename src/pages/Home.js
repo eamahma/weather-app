c3742ad03a4c7a36581c33bc92fce3d7c8676ffe
Amir Ahmadi <eamahma@gmail.com>
2021-11-10 20:41:31 -0500

    display API data

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,7 @@ import { GET_WEATHER_QUERY } from "../graphql/Queries";
 function Home() {
 
     const [citySearched, setCitySearched] = useState("");
-    const [getWeather, { loading, data, error }] = useLazyQuery(GET_WEATHER_QUERY, {
+    const [getWeather, {data, error} ] = useLazyQuery(GET_WEATHER_QUERY, {
         // variables: { name: "Vancouver"},
         variables: { name: citySearched},
     })
@@ -17,10 +17,26 @@ function Home() {
     return (
         <div className="home">
             <h1>Search for Weather</h1>
-            <input type="text" placeholder="City name ...." onChange = {(event) => {setCitySearched(event.target.value)}}/>      
-            <button onClick={() => getWeather() }>Search</button>      
+            <input 
+                type="text"
+                placeholder="City name ...."
+                onChange = {(event) => {
+                    setCitySearched(event.target.value);
+                    }
+                }/>      
+            <button onClick={() => getWeather() }>Search</button>
+            <div className="Weather">
+                {data && (
+                    <>
+                    <h1> City: {data.getCityByName.name} </h1>
+                    <h1> Temperature: {data.getCityByName.weather.temperature.actual} </h1>
+                    <h1> Description: {data.getCityByName.weather.summary.description} </h1>
+                    <h1> Wind Speed: {data.getCityByName.weather.wind.speed} </h1>
+                    </>
+                )}
+            </div>    
         </div>
-    )
+    );
 }
 
 export default Home
